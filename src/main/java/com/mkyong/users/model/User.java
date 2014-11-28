7bbb4f547abfb61db08cc2b02355dabcdd3f6425
srampv <srampv@gmail.com>
2014-11-29 03:31:38 +0530

    added few changes

--- src/main/java/com/mkyong/users/model/User.java
+++ src/main/java/com/mkyong/users/model/User.java
@@ -13,70 +13,33 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
-@Entity
-@Table(name="users",catalog="seo")
-public class User{
-
-	private String username;
-	private String password;
-	private boolean enabled;
-	private Set<UserRole> userRole = new HashSet<UserRole>(0);
-	
-	
-    
-
-	public User() {
-	}
-
-	public User(String username, String password, boolean enabled) {
-		this.username = username;
-		this.password = password;
-		this.enabled = enabled;
-	}
-
-	public User(String username, String password, boolean enabled, Set<UserRole> userRole) {
-		this.username = username;
-		this.password = password;
-		this.enabled = enabled;
-		this.userRole = userRole;
-	}
-
-	@Id
-@Column(name="username")    
-	public String getUsername() {
-		return this.username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	@Column(name="password")
-	public String getPassword() {
-		return this.password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-    @Column(name="enabled")
-	public boolean isEnabled() {
-		return this.enabled;
-	}
-
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
-
-	@OneToMany(cascade={CascadeType.ALL})
-	@JoinColumn(name="username")
-	public Set<UserRole> getUserRole() {
-		return this.userRole;
-	}
-
-	public void setUserRole(Set<UserRole> userRole) {
-		this.userRole = userRole;
-	}
+public class User{private String username;
+private String password;
+private boolean enabled;
+private Set<UserRole> userRole = new HashSet<UserRole>(0);
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public boolean isEnabled() {
+	return enabled;
+}
+public void setEnabled(boolean enabled) {
+	this.enabled = enabled;
+}
+public Set<UserRole> getUserRole() {
+	return userRole;
+}
+public void setUserRole(Set<UserRole> userRole) {
+	this.userRole = userRole;
+}
 
 }
--- src/main/java/com/mkyong/users/model/UserRole.java
+++ src/main/java/com/mkyong/users/model/UserRole.java
@@ -7,49 +7,27 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-@Entity
-@Table(name="user_roles",catalog="seo")
 public class UserRole {
-
 	private Integer userRoleId;
 	private User user;
 	private String role;
-
-	public UserRole() {
-	}
-
-	public UserRole(User user, String role) {
-		this.user = user;
-		this.role = role;
-	}
-
-	@Id
-	@GeneratedValue
-	@Column(name="user_role_id")
 	public Integer getUserRoleId() {
-		return this.userRoleId;
+		return userRoleId;
 	}
-
 	public void setUserRoleId(Integer userRoleId) {
 		this.userRoleId = userRoleId;
 	}
-
-	@ManyToOne
 	public User getUser() {
-		return this.user;
+		return user;
 	}
-
 	public void setUser(User user) {
 		this.user = user;
 	}
-
-	@Column(name="ROLE")
 	public String getRole() {
-		return this.role;
+		return role;
 	}
-
 	public void setRole(String role) {
 		this.role = role;
 	}
-
+	
 }
